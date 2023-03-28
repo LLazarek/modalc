7d3a897d202aa5bbb69186d3502c5faf7c67c9df
Lukas Lazarek <LukasALazarek@gmail.com>
2023-03-28 14:22:26 -0500

    Restructure for packaging

---
+++ modalc-lib/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection "modalc")
+(define build-deps '("racket-doc"
+                     "scribble-lib"
+                     "at-exp-lib"))
+(define deps '("base"
+               "https://github.com/LLazarek/ruinit.git"))
+
+(define scribblings '())
+
---
+++ modalc-lib/main.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(require "modal.rkt"
+         "modes.rkt")
+
+(provide (all-from-out "modal.rkt")
+         (all-from-out "modes.rkt"))
