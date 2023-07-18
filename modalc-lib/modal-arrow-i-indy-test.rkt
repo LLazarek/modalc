62168f8fbceabd8861853c75f8d60be940c9df25
Lukas Lazarek <LukasALazarek@gmail.com>
2023-07-18 13:27:27 -0500

    modal->i: switch to modified copy of normal impl, but 'lax' wrt modes
    That is, modes receive un-protected arguments to the function.
    
    It would take a good bit of actual effort to get the proper indy semantics
    working for modes, and I see little point right now.

--- modalc-lib/modal-arrow-i-indy-test.rkt
+++ modalc-lib/modal-arrow-i-indy-test.rkt
@@ -2,7 +2,91 @@
 
 (require ruinit
          "modal.rkt"
-         "test-common.rkt")
+         "modes.rkt"
+         "test-common.rkt"
+         "modal-arrow-i.rkt")
+
+(module basics racket
+  (require ruinit
+           "modal.rkt"
+           "modes.rkt"
+           "test-common.rkt"
+           "modal-arrow-i.rkt")
+  (test-begin
+    #:name modal->i-emulates-->i
+    (ignore (define/contract (f-n-any x)
+              (modal->i mode:always
+                        ([x number?])
+                        any)
+              x)
+            (define/contract (f-n-str x)
+              (modal->i mode:always ([x number?]) [result string?])
+              x)
+            (define/contract (f-n-= x)
+              (modal->i mode:always ([x number?]) [result {x} (=/c x)])
+              x)
+            (define/contract (f-n->-= x y)
+              (modal->i mode:always ([x number?] [y {x} (>/c x)]) [result {y} (=/c y)])
+              x)
+            (define/contract (f-n->-=/kw x #:y y)
+              (modal->i mode:always
+                        ([x number?]
+                         #:y [y {x} (>/c x)])
+                        [result {y} (=/c y)])
+              x))
+    (test-equal? (f-n-any 5) 5)
+    (test-blamed (f-n-any 'hi)
+                 (list (? path-string?) 'basics))
+    (test-blamed (f-n-str 5)
+                 '(function f-n-str))
+    (test-equal? (f-n-= 5) 5)
+    (test-blamed (f-n->-= 5 6)
+                 '(function f-n->-=))
+    (test-blamed (f-n->-= 5 3)
+                 (list (? path-string?) 'basics))
+    (test-blamed (f-n->-=/kw 5 #:y 3)
+                 (list (? path-string?) 'basics)))
+
+  (test-begin
+    #:name modal->i-modes
+    (ignore (define/contract (f-modal-> x)
+              (modal->i (mode:once-every 2)
+                        ([x number?])
+                        [result {x} (and/c number?
+                                           (=/c x))])
+              x)
+            (define/contract (g-modal-> x)
+              (modal->i (mode:once-every 2)
+                        ([x number?])
+                        [result {x} (and/c number?
+                                           (=/c x))])
+              (add1 x)))
+    (test-equal? (f-modal-> 2) 2)
+    (test-equal? (f-modal-> 2) 2)
+    (test-exn exn:fail:contract:blame?
+              (f-modal-> "hi"))
+    (test-equal? (f-modal-> "hi") "hi")
+    (test-exn exn:fail:contract:blame?
+              (f-modal-> "hi"))
+    (test-equal? (f-modal-> "hi") "hi")
+
+    (test-exn exn:fail:contract:blame?
+              (g-modal-> 2))
+    (test-equal? (g-modal-> 2) 3))
+
+  (test-begin
+    #:name modal->i-post
+    (ignore (define/contract (f-modal-> x)
+              (modal->i (mode:once-every 2)
+                        ([x number?])
+                        [result {x} (and/c number?
+                                           (=/c x))]
+                        #:post {x result} (> result x))
+              x))
+    (test-exn exn:fail:contract:blame?
+              (f-modal-> 2))
+    (test-equal? (f-modal-> 2) 2)))
+(require 'basics)
 
 (module indy-test-submod racket
   (require "modal.rkt")
