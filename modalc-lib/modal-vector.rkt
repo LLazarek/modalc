146542c2c1a0547127aa15e0a2d32672ba70d88e
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2024-02-19 19:20:49 -0600

    ref, set keyword implementation

--- modalc-lib/modal-vector.rkt
+++ modalc-lib/modal-vector.rkt
@@ -1,30 +1,11 @@
 #lang racket
 
-(require modalc)
+(require modalc
+         syntax/parse/define)
 
 (define (modal-vector/c should-apply-ctc? . inner-ctc)
   (define inner-ctc-proj
     (map contract-late-neg-projection inner-ctc))
-  #;(define r
-    (impersonate-procedure
-     should-apply-ctc?
-     (cond [(integer? (procedure-arity should-apply-ctc?))
-            (λ args
-              (println args)
-              (values (λ (res)
-                        (println res)
-                        res)
-                      args))]
-           [else (λ ()
-                   (values (λ (res)
-                             (println res))))])))
-  #;(define (s n)
-    (impersonate-procedure
-     should-apply-ctc?
-     (λ x
-       (values (λ (res)
-                 res)
-               x))))
   (make-contract
    #:name `(modal-vector/c ,(map contract-name inner-ctc-proj))
    #:late-neg-projection
@@ -52,9 +33,49 @@
                                    [else (printf "no set ~s\n" (list index value))
                                          value])))))))
 
-#;(define/contract vec
-  (modal-vector/c (mode:first 5) integer? integer? (and/c integer? positive?))
-  (vector 1 2 1))
+(define-simple-macro (modal-vector/c* mode
+                                 {~and inner-ctc {~not _:keyword}} ...
+                                 {~seq kw:keyword ctc} ...)
+  (make-modal-vector/c* mode
+                   (list inner-ctc ...)
+                   (list (list 'kw ctc) ...)))
+
+(define (make-modal-vector/c* should-apply-ctc? inner-ctc kwds)
+  (define ref-ctc? should-apply-ctc?)
+  (define set-ctc? should-apply-ctc?)
+  (for ([kw kwds])
+    (cond [(equal? '#:ref (car kw))
+           (set! ref-ctc? (car (cdr kw)))]
+          [(equal? '#:set (car kw))
+           (set! set-ctc? (car (cdr kw)))]))
+  (define inner-ctc-proj
+    (map contract-late-neg-projection inner-ctc))
+  (make-contract
+   #:name `(modal-vector/c ,(map contract-name inner-ctc-proj))
+   #:late-neg-projection
+   (λ (blame)
+     (define inner-ctc-proj/blame (map (λ (p) (p (blame-swap blame))) inner-ctc-proj))
+     (λ (val neg-party)
+       (impersonate-vector val
+                           (λ (vec index value)
+                             (cond [(ref-ctc? (list index value))
+                                    (printf "ref call: ~s\n" (list index value))
+                                    (for/list ([arg (in-vector vec)]
+                                               [proj (in-list inner-ctc-proj/blame)])
+                                      (proj arg neg-party))
+                                    value]
+                                   [else (printf "no ref ~s\n" (list index value))
+                                         value]))
+                           (λ (vec index value)
+                             (cond [(set-ctc? (list index value))
+                                    (printf "set call: ~s\n" (list index value))
+                                    (for/list ([arg (in-vector vec)]
+                                               [proj (in-list inner-ctc-proj/blame)])
+                                      (proj arg neg-party))
+                                    (vector-set! vec index value)
+                                    value]
+                                   [else (printf "no set ~s\n" (list index value))
+                                         value])))))))
 
 (module+ test
   (require ruinit
@@ -65,7 +86,7 @@
     (modal-vector/c mode:always integer? integer?)
     (vector 1 2))
   (define/contract every-other
-    (modal-vector/c (mode:once-every 2) integer? integer? (and/c positive? integer?))
+    (modal-vector/c* (mode:once-every 2) integer? integer? (and/c positive? integer?) #:ref mode:always #:set (mode:first 2))
     (vector 4 5 6))
 
   #;(test-begin
@@ -85,8 +106,10 @@
    #;(test-equal? every-other (vector 4 5 6))
    (println "next")
    (test-equal? (vector-ref every-other 0) 4)
-   ;(test-equal? (vector-ref every-other 1) 5)
+   (test-equal? (vector-ref every-other 1) 5)
    (vector-set! every-other 1 -2)
+   (vector-set! every-other 2 2)
+   (vector-set! every-other 0 12)
    ;(test-equal? (vector-ref every-other 2) 6)
    ;(test-equal? (vector-ref every-other 1) -2)
    ;(vector-set! every-other 2 0)
