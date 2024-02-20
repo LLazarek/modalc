2a9b32d042b6ef1f98d7af0fa50b6fe713fae1c6
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2024-02-19 18:20:19 -0600

    fixed implementation

--- modalc-lib/modal-vector.rkt
+++ modalc-lib/modal-vector.rkt
@@ -3,43 +3,93 @@
 (require modalc)
 
 (define (modal-vector/c should-apply-ctc? . inner-ctc)
-  (define vector-contract (apply vector/c inner-ctc))
   (define inner-ctc-proj
-    (contract-late-neg-projection vector-contract))
+    (map contract-late-neg-projection inner-ctc))
+  #;(define r
+    (impersonate-procedure
+     should-apply-ctc?
+     (cond [(integer? (procedure-arity should-apply-ctc?))
+            (λ args
+              (println args)
+              (values (λ (res)
+                        (println res)
+                        res)
+                      args))]
+           [else (λ ()
+                   (values (λ (res)
+                             (println res))))])))
+  #;(define (s n)
+    (impersonate-procedure
+     should-apply-ctc?
+     (λ x
+       (values (λ (res)
+                 res)
+               x))))
   (make-contract
-   #:name `(modal-vector/c ,(contract-name vector-contract))
+   #:name `(modal-vector/c ,(map contract-name inner-ctc-proj))
    #:late-neg-projection
    (λ (blame)
-     (define inner-ctc-proj/blame (inner-ctc-proj blame))
+     (define inner-ctc-proj/blame (map (λ (p) (p (blame-swap blame))) inner-ctc-proj))
      (λ (val neg-party)
        (impersonate-vector val
-                           (λ (vec index ret)
-                             (if (should-apply-ctc? val)
-                                 (vector-ref (inner-ctc-proj/blame val neg-party) index)
-                                 ret))
-                           (λ (vec index ret)
-                             (if (should-apply-ctc? val)
-                                 (begin (inner-ctc-proj/blame val neg-party)
-                                        ret)
-                                 ret)))))))
+                           (λ (vec index value)
+                             (cond [(should-apply-ctc? (list index value))
+                                    (printf "ref call: ~s\n" (list index value))
+                                    (for/list ([arg (in-vector vec)]
+                                               [proj (in-list inner-ctc-proj/blame)])
+                                      (proj arg neg-party))
+                                    value]
+                                   [else (printf "no ref ~s\n" (list index value))
+                                         value]))
+                           (λ (vec index value)
+                             (cond [(should-apply-ctc? (list index value))
+                                    (printf "set call: ~s\n" (list index value))
+                                    (for/list ([arg (in-vector vec)]
+                                               [proj (in-list inner-ctc-proj/blame)])
+                                      (proj arg neg-party))
+                                    (vector-set! vec index value)
+                                    value]
+                                   [else (printf "no set ~s\n" (list index value))
+                                         value])))))))
 
-(define/contract vec
+#;(define/contract vec
   (modal-vector/c (mode:first 5) integer? integer? (and/c integer? positive?))
   (vector 1 2 1))
 
-(define (f v)
-  (println "start")
-  (println (vector-ref v 0))
-  (println (vector-ref v 1))
-  (println (vector-ref v 2))
-  (vector-set! v 2 -5)
-  (println "vector-set")
-  (println (vector-ref v 0))
-  (println (vector-ref v 1))
-  (println (vector-ref v 2))
-  (println v)
-  (println "end")
-  (if (vector? v)
-      (println "is a vector")
-      (println "wrong")))
-(f vec)
+(module+ test
+  (require ruinit
+           "test-common.rkt")
+  
+  
+  (define/contract simple-vec
+    (modal-vector/c mode:always integer? integer?)
+    (vector 1 2))
+  (define/contract every-other
+    (modal-vector/c (mode:once-every 2) integer? integer? (and/c positive? integer?))
+    (vector 4 5 6))
+
+  #;(test-begin
+    #:name simple-test
+    (test-equal? simple-vec (vector 1 2))
+    (test-equal? (vector-ref simple-vec 1) 2)
+    (vector-set! simple-vec 1 0)
+    (test-equal? (vector-ref simple-vec 1) 0)
+    (vector-set! simple-vec 0 'bad)
+    (test-exn exn:fail:contract:blame? (vector-ref simple-vec 0))
+    (test-exn exn:fail:contract:blame? (vector-set! simple-vec 1 #f))
+    (test-exn exn:fail:contract:blame? (vector-ref simple-vec 1))
+    (test-exn exn:fail:contract:blame? (vector-set! simple-vec 0 -10))
+    (test-exn exn:fail:contract:blame? (vector-set! simple-vec 1 15)))
+  (test-begin
+   #:name every-other-test
+   #;(test-equal? every-other (vector 4 5 6))
+   (println "next")
+   (test-equal? (vector-ref every-other 0) 4)
+   ;(test-equal? (vector-ref every-other 1) 5)
+   (vector-set! every-other 1 -2)
+   ;(test-equal? (vector-ref every-other 2) 6)
+   ;(test-equal? (vector-ref every-other 1) -2)
+   ;(vector-set! every-other 2 0)
+   #;(test-equal? every-other (vector 4 -2 6))
+   #;(test-exn exn:fail:contract:blame? (vector-ref every-other 2))))
+
